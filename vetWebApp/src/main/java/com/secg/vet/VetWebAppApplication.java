f41e5b07b9062924d4d53c9db17e5728ad053768
salvadorc94 <secg.1994@gmail.com>
2019-10-05 13:44:50 -0600

    Initial Commit Estructura de App y dependencias

---
+++ vetWebApp/src/main/java/com/secg/vet/VetWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.secg.vet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetWebAppApplication.class, args);
+	}
+
+}
---
+++ vetWebApp/src/test/java/com/secg/vet/VetWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.secg.vet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VetWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
